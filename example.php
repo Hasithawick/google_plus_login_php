0e36abfbf375b394290f948a05db059e9cc3be69
Hasitha Wickramanayaka <hasithawickramanayaka@gmail.com>
2017-06-26 00:51:23 +0530

    v2.0

--- example.php
+++ example.php
@@ -1,18 +1,18 @@
 <?php
 /* * Copyright 2011 Google Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 //replace --- with your own api and secret keys obtained while registgering for your new app in Google API Console
 //require_once 'google-api-php-client/src/apiClient.php';
 //require_once 'google-api-php-client/src/contrib/apiPlusService.php';
@@ -31,61 +31,61 @@ $client->setRedirectUri('http://www.w3resource.com/API/google-plus/example.php')
 $plus = new apiPlusService($client);
 
 if (isset($_REQUEST['logout'])) {
-unset($_SESSION['access_token']);
+    unset($_SESSION['access_token']);
 }
 
 if (isset($_GET['code'])) {
-$client->authenticate();
-$_SESSION['access_token'] = $client->getAccessToken();
-header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+    $client->authenticate();
+    $_SESSION['access_token'] = $client->getAccessToken();
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
 }
 
 if (isset($_SESSION['access_token'])) {
-$client->setAccessToken($_SESSION['access_token']);
+    $client->setAccessToken($_SESSION['access_token']);
 }
 
 if ($client->getAccessToken()) {
-$me = $plus->people->get($id);
+    $me = $plus->people->get($id);
 
-$optParams = array('maxResults' => 100);
-$activities = $plus->activities->listActivities($id, 'public', $optParams);
+    $optParams = array('maxResults' => 100);
+    $activities = $plus->activities->listActivities($id, 'public', $optParams);
 
 // The access token may have been updated lazily.
-$_SESSION['access_token'] = $client->getAccessToken();
+    $_SESSION['access_token'] = $client->getAccessToken();
 } else {
-$authUrl = $client->createAuthUrl();
+    $authUrl = $client->createAuthUrl();
 }
 ?>
 
 <!doctype html>
 <html>
-<head><link rel='stylesheet' href='style.css' /></head>
-<body>
-<header><h1>Google+ Sample App</h1></header>
-<div class="box">
+    <head><link rel='stylesheet' href='style.css' /></head>
+    <body>
+        <header><h1>Google+ Sample App</h1></header>
+        <div class="box">
 
-<?php if(isset($me) && isset($activities)): ?>
-<div class="me">
-<p><a rel="me" href="<?php echo $me['url'] ?>"><?php print $me['displayName'] ?></a></p>
-<p><?php print $me['tagline'] ?></p> 
-<p><?php print $me['aboutMe'] ?></p> 
-<div><img src="<?php echo $me['image']['url']; ?>?sz=82" /></div>
-</div>
-<div class="activities">Your Activities:
-<?php foreach($activities['items'] as $activity): ?>
-<div class="activity">
-<p><a href="<?php print $activity['url'] ?>"><?php print $activity['title'] ?></a></p>
-</div>
-<?php endforeach ?>
-</div>
-<?php endif ?>
-<?php
-if(isset($authUrl)) {
-print "<a class='login' href='$authUrl'>Connect Me!</a>";
-} else {
+            <?php if (isset($me) && isset($activities)): ?>
+                <div class="me">
+                    <p><a rel="me" href="<?php echo $me['url'] ?>"><?php print $me['displayName'] ?></a></p>
+                    <p><?php print $me['tagline'] ?></p> 
+                    <p><?php print $me['aboutMe'] ?></p> 
+                    <div><img src="<?php echo $me['image']['url']; ?>?sz=82" /></div>
+                </div>
+                <div class="activities">Your Activities:
+                    <?php foreach ($activities['items'] as $activity): ?>
+                        <div class="activity">
+                            <p><a href="<?php print $activity['url'] ?>"><?php print $activity['title'] ?></a></p>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+            <?php endif ?>
+            <?php
+            if (isset($authUrl)) {
+                print "<a class='login' href='$authUrl'>Connect Me!</a>";
+            } else {
 //print "<a class='logout' href='?logout'>Logout</a>";
-}
-?>
-</div>
-</body>
+            }
+            ?>
+        </div>
+    </body>
 </html>
